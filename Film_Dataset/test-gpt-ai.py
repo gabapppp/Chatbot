60ac001ae27d83a3e81d428fd649290111078c24
gabapppp <khiempham201@gmail.com>
2025-01-08 01:34:26 +0700

    init

---
+++ Film_Dataset/test-gpt-ai.py
@@ -0,0 +1,33 @@
+import openai
+
+response = openai.ChatCompletion.create(
+    model="gpt-4o",
+    messages=[
+        {
+            "role": "system", 
+            "content": "You extract email addresses into JSON data."
+        },
+        {
+            "role": "user", 
+            "content": "Feeling stuck? Send a message to help@mycompany.com."
+        }
+    ],
+    response_format={
+        "type": "json_schema",
+        "json_schema": {
+            "name": "email_schema",
+            "schema": {
+                "type": "object",
+                "properties": {
+                    "email": {
+                        "description": "The email address that appears in the input",
+                        "type": "string"
+                    },
+                    "additionalProperties": False
+                }
+            }
+        }
+    }
+)
+
+print(response.choices[0].message.content)
